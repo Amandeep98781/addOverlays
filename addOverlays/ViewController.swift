27896e2b3213fc3b66947cbcea5ad0c6df25c3d9
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-01-25 10:08:13 -0500

    hello

--- addOverlays/ViewController.swift
+++ addOverlays/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController , CLLocationManagerDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var map: MKMapView!
+  
+    var locationManager = CLLocationManager()
+   
+    let places = Place.getPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+       locationManager.delegate = self
+       locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        let latitude: CLLocationDegrees = 43.64
+        let longitude: CLLocationDegrees = -79.38
+        
+       map.delegate = self
+        
+       addAnnotationForPlaces()
+        
         // Do any additional setup after loading the view.
+   addPolyline()
     }
-
-
+   
+    func addAnnotationForPlaces(){
+        map.addAnnotations(places)
+        let overlays = places.map {MKCircle(center: $0.coordinate, radius: 2000)}
+        map.addOverlays(overlays)
+    }
+    
+    
+    func addPolyline() {
+    let coordinates = places.map {$0.coordinate}
+    let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+    map.addOverlay(polyline)
 }
 
+}
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            renderer.strokeColor = UIColor.green
+            renderer.lineWidth = 3
+            return renderer
+            
+        }
+        return MKOverlayRenderer()
+        
+    }
+    
+    
+    
+    
+    
+}
